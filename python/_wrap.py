234bac6b1ac27c7b7fce3469f02934a65bdcd8d9
Edwin J. Son <eddy@nims.re.kr>
2016-11-24 23:40:12 +0900

    fix a bug of undefined arange by adding arange in import from numpy

--- python/_wrap.py
+++ python/_wrap.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 
 import _etagen
-from numpy import array, hstack, vstack, loadtxt, median
+from numpy import array, arange, hstack, vstack, loadtxt, median
 from matplotlib.pyplot import scatter
 
 class etagen(_etagen.etagen):
